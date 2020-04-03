465afe9676f00a190fc49d2bd0d1e7ea0f1703c2
art <art>
2020-04-03 13:41:12 +0300

    default path delete, option added

--- src/main/scala/JsonReader.scala
+++ src/main/scala/JsonReader.scala
@@ -10,7 +10,7 @@ import org.apache.spark.SparkConf
 case class JsonData(country:String,
                         id: BigInt, 
                         points: BigInt, 
-                        price: Double, 
+                        price: Option[Double], 
                         title: String, 
                         variety: String, 
                         winery: String)
@@ -20,12 +20,15 @@ object JsonReader extends App {
         val conf = new SparkConf().setAppName("JsonReader").setMaster("local")
         val sc = new SparkContext(conf)
         
-        val path: String = "file:///data/winemag-data-130k-v2.json"
+        //val path: String = "file:///data/winemag-data-130k-v2.json"
         val json = sc.textFile(path)
         implicit val formats = DefaultFormats
-
+		
+		println("testsss ---------->")
+		
         val result = json.map(s => parse(s).extract[JsonData])
-        result.foreach(line => println(line.toString))
+        result.take(30).foreach(println)
+			//(line => println(line.toString))
             //result.take(n).foreach(println)
     }
 }
